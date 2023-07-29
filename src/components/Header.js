68765d3931387595f47a6259a66c777876eb1154
Azikenp <azikenp26@gmail.com>
2023-07-29 20:29:16 +0100

    new files

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header(){
+    return (
+        <header>
+            <h2>Header Component</h2>
+            <h4>React Course - Project 3</h4>
+        </header>
+    )
+}
