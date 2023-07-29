00c29d3e8a0e856beea1c8d2ae69cce236ec738f
Azikenp <azikenp26@gmail.com>
2023-07-29 21:39:12 +0100

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
+import logo from '../troll-face.png'
 
 export default function Header(){
     return (
-        <header>
-            <h2>Header Component</h2>
-            <h4>React Course - Project 3</h4>
+        <header className="header">
+            <img className="header--image" src={logo}  alt="logo"/>
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
         </header>
     )
 }
---
+++ src/components/Meme.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Meme(){
+    return (
+        <main>
+            <form className="form">
+                <input className="form--inputs" type="text" placeholder="top image" />
+                <input className="form--inputs" type="text"  placeholder="bottom image"/>
+                <button className="form--btn">Genrate a new meme imagee</button>
+            </form>
+        </main>
+    )
+}
